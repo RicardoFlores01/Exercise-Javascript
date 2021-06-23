0f8f7f1960376521abf276dbbf62d33ec9a4d824
Ricardo Flores <48632924+RicardoFlores01@users.noreply.github.com>
2021-06-23 15:13:23 -0500

    Add files via upload
    Add files

---
+++ operadores.js
@@ -0,0 +1,8 @@
+// OPERADORES TERNARIOS, ? devuelve un true, : devuelve false
+let age = prompt("Ingresa tu edad")
+
+let isAdult = age >= 18
+				? 'Eres un mayor de edad'
+				: 'Aun eres un nino'
+
+alert(isAdult)
